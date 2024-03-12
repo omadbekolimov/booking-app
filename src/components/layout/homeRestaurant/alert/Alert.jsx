e0444fd66f014b949b7168f9ec7d8c67f8bb1af6
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-03-12 14:34:35 +0500

    navbar olvolilar

---
+++ src/components/layout/homeRestaurant/alert/Alert.jsx
@@ -0,0 +1,16 @@
+
+const Alert = () => {
+    return (
+        <section className="max-w-[1100px] rounded-[20px] py-8 px-10 md:px-16 bg-[#F1E8D7] mx-auto shadow-lg mt-[-6rem]">
+            <h1 className="font-signika text-[2.75rem]">Soy asian Restaurant</h1>
+            <div className="px-8">
+                <hr className="border-t-2 border-[#F46A06] w-3/4 my-4" />
+                <h2 className="text-[#F46A06] font-quicksand font-semibold text-4xl">Restaurant description</h2>
+                <h5 className="font-signika text-2xl py-3 text-[#1F1C1C]">Soy asian table restaurant is a restaurant located in the heart
+                    of kimihurura it was b rought by japanese to bring the taste of asian food in africa.</h5>
+            </div>
+        </section>
+    )
+}
+
+export default Alert
--- src/components/layout/homeRestaurant/navbar/Navbar.jsx
+++ src/components/layout/homeRestaurant/navbar/Navbar.jsx
@@ -1,21 +1,81 @@
-import React from 'react'
+// styles
 import { styles } from '../../../../util/style'
+
+// icons
 import { FaHome } from "react-icons/fa";
+import { FaBowlFood } from "react-icons/fa6";
+import { IoCall } from "react-icons/io5";
+import { FiLogIn } from "react-icons/fi";
+import { FaList } from "react-icons/fa6";
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [navbar, setNavbar] = useState(false);
+
+  const toggleNavbar = () => setNavbar(!navbar);
+
   return (
     <>
-      <section className='w-full bg-black'>
-        <header className={`${styles.container} py-8 flex justify-around`}>
-          <h1 className='text-white text-3xl'>HFfinder</h1>
-          <nav className='w-full'>
-            <ul className='flex '>
-              <li>
-              <FaHome />
+      <section className='w-full fixed z-10 bg-white shadow-md'>
+        {/* header */}
+        <header className={`${styles.container} px-5 py-8 md:px-0 flex items-center justify-between`}>
+          {/* logo */}
+          <h1 className='text-4xl cursor-pointer'>HFfinder</h1>
+          {/* / logo */}
+
+          {/* navbar */}
+          <nav className='text-[#F46A06] hidden md:block px-2 lg:px-0'>
+            <ul className='flex gap-8 lg:gap-12 font-semibold'>
+              <li className='flex items-center text-xl gap-2 cursor-pointer'>
+                <FaHome color='black' />
+                Home
+              </li>
+              <li className='flex items-center text-xl gap-2 cursor-pointer'>
+                <FaBowlFood color='black' />
+                Create facility
+              </li>
+              <li className='flex items-center text-xl gap-2 cursor-pointer'>
+                <IoCall color='black' />
+                ContactUs
+              </li>
+              <li className='flex items-center text-xl gap-2 cursor-pointer'>
+                <FiLogIn color='black' />
+                Login
               </li>
             </ul>
           </nav>
+          {/* / navbar */}
+          {/* navbar logo for sm */}
+          <FaList onClick={toggleNavbar} fontSize={30} className='block md:hidden cursor-pointer' />
+          {/* / navbar logo for sm */}
         </header>
+
+
+        {/* navbar sm */}
+        <nav className={`text-[#F46A06] duration-200 ${navbar ? "h-[230px]" : "h-0"} overflow-hidden block md:hidden px-2 lg:px-0`}>
+          <ul className='flex text-center flex-col gap-8 lg:gap-12 font-semibold'>
+            <li className='flex items-center text-xl gap-2 cursor-pointer'>
+              <FaHome color='black' />
+              Home
+            </li>
+            <li className='flex items-center text-xl gap-2 cursor-pointer'>
+              <FaBowlFood color='black' />
+              Create facility
+            </li>
+            <li className='flex items-center text-xl gap-2 cursor-pointer'>
+              <IoCall color='black' />
+              ContactUs
+            </li>
+            <li className='flex items-center text-xl gap-2 cursor-pointer'>
+              <FiLogIn color='black' />
+              Login
+            </li>
+          </ul>
+        </nav>
+        {/* / navbar sm */}
+
+
       </section>
     </>
   )
