d8a77fb0ecb6086ab633a4f006ac00a3663d9d30
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-03-12 13:42:35 +0500

    add modal component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./app";
+import { MainHotelDashboard } from "./pages/main-hotel-dashboard";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MainHotelDashboard />
   </React.StrictMode>
   )
