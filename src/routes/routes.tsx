44653fd0538493ae21790c9e964134eea89f69c8
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-03-12 10:45:56 +0500

    file srtucture new version

---
+++ src/routes/routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Route, Routes as Switch } from "react-router-dom";
+import { App } from "../pages";
+
+interface RouterProps {}
+
+const Router: React.FC<RouterProps> = (props) => {
+	return (
+		<div>
+			<Switch>
+				<Route index path="/about" element={<App.AdminD />} />
+			</Switch>
+		</div>
+	);
+};
+
+export default Router;
